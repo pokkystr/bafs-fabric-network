c1c85efa8f88aae05be26dd6d54afb43c1a0db36
pigke <pokky_str@hotmail.com>
2019-09-11 12:25:43 +0700

    delete pem

--- bafsorg/join-org-script.sh
+++ bafsorg/join-org-script.sh
@@ -1,10 +1,20 @@
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/crypto-config/peerOrganizations/bafsorg.com/users/Admin@bafsorg.com/msp
-export CORE_PEER_ADDRESS=peer0.bafsorg.com:7051
-export CORE_PEER_LOCALMSPID="bafsorg"
-export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/crypto-config/peerOrganizations/bafsorg.com/peers/peer0.bafsorg.com/tls/ca.crt
-export ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/crypto-config/ordererOrganizations/ordererOrganizations/orderer.bafs.com/msp/tlscacerts/tlsca.orderer.bafs.com-cert.pem
-export CHANNEL_ID=tradeoil-channel
-export ORDERER_ADDRESS=34.92.224.227:7050
 
-peer channel create -o $ORDERER_ADDRESS -c $CHANNEL_ID -f tradeOilChannel.tx --tls --cafile $ORDERER_CA
+export CORE_PEER_LOCALMSPID="bafsorgmsp"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/channel-artifacts/crypto-config/peerOrganizations/bafsorg.com/peers/peer0.bafsorg.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/channel-artifacts/crypto-config/peerOrganizations/bafsorg.com/users/Admin@bafsorg.com/msp
+export CORE_PEER_ADDRESS=peer0.bafsorg.com:7051
+export ORDERER_CA=${PWD}/channel-artifacts/crypto-config/ordererOrganizations/orderer.bafs.com/msp/tlscacerts/tlsca.orderer.bafs.com-cert.pem
+
+export CHANNEL_ID=tradeoil-channel
+export ORDERER_ADDRESS=10.146.0.4:7050
+
+
+# peer channel create -o orderer.example.com:7050 -c tradeoil-channel -f ./channel-artifacts/channel.tx >&log.txt
+peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
+
+peer channel create -o 10.146.0.4:7050 -c tradeoil-channel -f bafsorgmspanchors.tx --tls --cafile $ORDERER_CA
+peer channel create -o 10.146.0.4:7050 -c tradeoil-channel -f bafsorgmspanchors.tx >&log.txt
+
+peer channel create -o orderer.bafs.com:7050 -c tradeoil-channel -f ./channel-artifacts/bafsorgmspanchors.tx --tls --cafile $ORDERER_CA
+
 peer channel join -b $CHANNEL_ID.block
