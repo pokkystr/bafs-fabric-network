267784380885bf33a4bf0e1bda8863deacaa850a
pigke <pokky_str@hotmail.com>
2019-09-22 16:42:57 +0700

    add sh create network orderer

--- orderer/start-network.sh
+++ orderer/start-network.sh
@@ -3,9 +3,12 @@
 MODE=$1
 shift
 if [ "$MODE" == "up" ]; then
+    docker network rm orderer
+    docker network create -d bridge orderer
     docker-compose -f orderer-compose.yaml  up
 elif [ "$MODE" == "down" ]; then
     docker-compose -f orderer-compose.yaml down --volumes --remove-orphans
+    docker network rm orderer
     docker rm $(docker ps -a -f status=exited -q)
 else 
   echo "Please input up or down"
